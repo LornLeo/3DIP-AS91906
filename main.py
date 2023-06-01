d2d508cd80efd090097a27bd4ee8e12b82cf1ea2
LornLeo <134342527+LornLeo@users.noreply.github.com>
2023-06-01 02:48:59 +0000

    set up  the initial interface

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("test")
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+window.geometry('1000x600')
+window.title("Menu")
+window.resizable(0, 0)
+window.configure(bg="white")
+window.mainloop()
