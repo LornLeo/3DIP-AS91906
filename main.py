596924fbac31c7d2fcacd51dae893d8847359eeb
LornLeo <134342527+LornLeo@users.noreply.github.com>
2023-06-01 21:55:57 +0000

    add the title of the menu

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ window.geometry('1000x600')
 window.title("Menu")
 window.resizable(0, 0)
 window.configure(bg="white")
+lblun=Label(window,text="Our Menu ",font=('Rockwell 20'),bg='#fcc302',fg="white",width=12,padx=30,anchor=W)
+lblun.grid(row=0,column=0,pady=30)
+
+
+
+
+
 window.mainloop()
