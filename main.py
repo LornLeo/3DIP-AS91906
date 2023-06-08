46bf674ee3977e648b02ce884656b23462c77ae9
LornLeo <134342527+LornLeo@users.noreply.github.com>
2023-06-08 02:47:01 +0000

    Frame the labels

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ window.geometry('1000x600')
 window.title("Menu")
 window.resizable(0, 0)
 window.configure(bg="white")
-lblun=Label(window,text="Our Menu ",font=('Rockwell 20'),bg='#fcc302',fg="white",width=12,padx=30,anchor=W)
-lblun.grid(row=0,column=0,pady=30)
-
-
-
-
+left_frame=Frame(window).pack(side="left")
+menu_title=Label(left_frame,text="Our Menu ",font=('Rockwell 20'),bg='#fcc302',fg="white",width=11,padx=30,pady=3,justify="left")
+menu_title.pack(padx=0,anchor=NW)
 
 window.mainloop()
